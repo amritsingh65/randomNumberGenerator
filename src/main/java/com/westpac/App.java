9af3bfc9ac2d58282f4aaf23dfa4a387bccbbd8b
Amrit Singh <amrit.saundh@gmail.com>
2023-06-28 08:50:28 +1200

    feature/randomNumberGenerator

---
+++ src/main/java/com/westpac/App.java
@@ -0,0 +1,17 @@
+package com.westpac;
+
+/**
+ * 
+ * EntryPoint of the application
+ *
+ * 
+ * @author : Amritpal Singh
+ * @github username : amritsingh65
+ * 
+ *
+ */
+public class App {
+	public static void main(String[] args) {
+		
+	}
+}
