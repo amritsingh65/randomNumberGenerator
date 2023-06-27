df36d4d46af9795dd84b185591f826b82b8854ba
Amrit Singh <amrit.saundh@gmail.com>
2023-06-28 08:53:53 +1200

    feature/randomNumberGenerator: added implementation for generating numbers

--- src/main/java/com/westpac/App.java
+++ src/main/java/com/westpac/App.java
@@ -12,6 +12,8 @@ package com.westpac;
  */
 public class App {
 	public static void main(String[] args) {
-		
+		RandomNumberGeneratorController controller = new RandomNumberGeneratorController();
+		RandomNumberGenerator numberGenerator = controller.getNumberGenerator();
+		numberGenerator.generate(1000, 1000, 9999).forEach(System.out::println);
 	}
 }
